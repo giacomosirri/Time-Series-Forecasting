ae8cd11f53939de33b6483db170b1987ed627964
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-04-11 11:38:42 +0200

    Add c# project files

---
+++ RealTests/Mnist/DatasetLoader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.ConstrainedExecution;
+using static TorchSharp.torch;
+using Python.Runtime;
+using System.Globalization;
+
+namespace Mnist
+{
+    internal class DatasetLoader
+    {
+        private readonly string _filePath;
+        private readonly FileStream _decompressedFile;
+
+        public DatasetLoader(string path)
+        {
+            PythonEngine.Initialize();
+            _filePath = path;
+            var newFilePath = Path.Combine(Environment.CurrentDirectory, "mnist.pkl");
+            if (!File.Exists(newFilePath))
+            {
+                _decompressedFile = File.Create(newFilePath);
+                using FileStream inputFile = File.OpenRead(_filePath);
+                using GZipStream decompressionStream = new(inputFile, CompressionMode.Decompress);
+                decompressionStream.CopyTo(_decompressedFile);
+            }
+            else
+            {
+                _decompressedFile = File.OpenRead(newFilePath);
+            }
+        }
+
+        public void LoadDataset()
+        {
+            using (Py.GIL())
+            {
+                dynamic pickle = Py.Import("pickle");
+                using BinaryReader binaryReader = new(_decompressedFile);
+                byte[] byteStream = Encoding.Convert(Encoding.Latin1, Encoding.ASCII, 
+                    binaryReader.ReadBytes((int)_decompressedFile.Length));
+                dynamic obj = pickle.loads(byteStream);
+                Console.WriteLine(obj.ToString(TorchSharp.TensorStringStyle.Default));
+            }
+        }
+    }
+}
---
+++ RealTests/Mnist/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TorchSharp;
+using static TorchSharp.torch;
+using static TorchSharp.TensorExtensionMethods;
+using static TorchSharp.torch.distributions;
+using System.Runtime.Serialization;
+using System.Transactions;
+using System.Text;
+using Python.Runtime;
+
+namespace Mnist
+{
+    internal class Program
+    {
+        
+        private static void WriteTensor(Tensor tensor) => Console.WriteLine(tensor.ToString(TorchSharp.TensorStringStyle.Default));
+
+        private static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", "C:\\Users\\sirri\\AppData\\Local\\Programs\\Python\\Python310\\python310.dll");
+            var trainImagesPath = Path.GetFullPath("mnist.pkl.gz");
+            DatasetLoader dl = new(trainImagesPath);
+            dl.LoadDataset();
+        }
+    }
+}
