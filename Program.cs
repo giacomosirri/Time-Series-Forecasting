22217b66eef547d8366205cc496dc8060bfd84e3
Giacomo Sirri <91158410+giacomosirri@users.noreply.github.com>
2023-04-12 15:27:36 +0200

    Add c# files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Parquet.Rows;
+using System;
+
+namespace RNN_Model_Creation
+{
+    internal class Program
+    {
+        //private static readonly string FILE_PATH = "C:\\WorkingCopy\\giacomo.sirri\\test-timeseries-pred\\dataset\\" +
+        //                                   "timeseries-2009-2016.snappy.parquet";
+        private static readonly string FILE_PATH = "C:\\WorkingCopy\\giacomo.sirri\\old-parquet-data\\df_join.parquet";
+
+        static void Main(string[] args)
+        {
+            new ParquetDataLoader(FILE_PATH).GetRecords().Wait();
+        }
+    }
+}
