6d79f68112d949cdf774b43276fa19735582b0c1
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-04-18 15:42:13 +0200

    Create interface IWindowGenerator

---
+++ RealTests/RNN_Model_Creation/IWindowGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using static TorchSharp.torch;
+
+namespace RNN_Model_Creation
+{
+    internal interface IWindowGenerator
+    {
+        /// <summary>
+        /// Splits the data into features and labels.
+        /// </summary>
+        /// <returns></returns>
+        public Tuple<Tensor, Tensor> GenerateWindows<T>(DataTable table);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public CustomDataset MakeDataset();
+    }
+}
