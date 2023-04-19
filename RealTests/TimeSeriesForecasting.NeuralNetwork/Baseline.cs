56082c0e6270502d9c0bcda21b051e5276aed6ec
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-04-19 14:27:13 +0200

    Begin implementation of neural network models

---
+++ RealTests/TimeSeriesForecasting.NeuralNetwork/Baseline.cs
@@ -0,0 +1,19 @@
+﻿using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+namespace TimeSeriesForecasting.NeuralNetwork
+{
+    public class Baseline : Module<Tensor, Tensor>
+    {
+        private readonly Module<Tensor, Tensor> layer;
+
+        public Baseline(int inputObservations, int inputFeatures, int outputObservations, int outputFeatures) 
+            : base(nameof(Baseline))
+        {
+            RegisterComponents();
+            layer = Linear(inputObservations * inputFeatures, outputObservations * outputFeatures);
+        }
+
+        public override Tensor forward(Tensor input) => layer.forward(input);
+    }
+}
---
+++ RealTests/TimeSeriesForecasting.NeuralNetwork/IModelTraining.cs
@@ -0,0 +1,13 @@
+﻿using static TorchSharp.torch.nn;
+
+namespace TimeSeriesForecasting.NeuralNetwork
+{
+    internal interface IModelTraining
+    {
+        Module Loss { get; set; }
+        Module Optimizer { get; set; }
+        double LearningRate { get; set; }
+
+        public void Fit();
+    }
+}
