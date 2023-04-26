f5feaa1bb3ee44132934fb062573e6cb0da8aa5e
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-04-26 17:05:25 +0200

    Complete implementation of TestWindowGenerator

--- RealTests/TestTimeSeriesForecasting/TestWindowGenerator.cs
+++ RealTests/TestTimeSeriesForecasting/TestWindowGenerator.cs
@@ -1,7 +1,5 @@
 ﻿using System.Data;
-using System.Transactions;
 using TimeSeriesForecasting;
-using TimeSeriesForecasting.IO;
 using static TorchSharp.torch;
 
 namespace TestTimeSeriesForecasting
@@ -12,8 +10,8 @@ namespace TestTimeSeriesForecasting
         private const int InputWidth = 10;
         private const int LabelWidth = 1;
         private const int Offset = 5;
-        private readonly string[] LabelColumns = new string[] { "D" };
-
+        
+        private readonly string[] _labelColumns = new string[] { "D" };
         private readonly PreprocessorFixture _fixture;
         private readonly DataTable _set;
         private readonly Tensor _input;
@@ -26,14 +24,14 @@ namespace TestTimeSeriesForecasting
         {
             _fixture = fixture;
             _set = fixture.Preprocessor.GetTrainingSet();
-            IWindowGenerator winGen = new WindowGenerator(InputWidth, LabelWidth, Offset, LabelColumns);
+            IWindowGenerator winGen = new WindowGenerator(InputWidth, LabelWidth, Offset, _labelColumns);
             (_input, _output) = winGen.GenerateWindows<double>(_set);
             // Number of batches = Rows - LabelWidth - Offset - InputWidth
             _batches = _set.Rows.Count - LabelWidth - Offset - InputWidth;            
             // Number of features per observation = Number of total features - Number of label columns - 1 (timestamp column)
-            _features = _set.Rows[0].ItemArray.Length - LabelColumns.Length - 1;
+            _features = _set.Rows[0].ItemArray.Length - _labelColumns.Length - 1;
             // Number of labels per observation = Number of LabelColumns
-            _labels = LabelColumns.Length;
+            _labels = _labelColumns.Length;
         }
 
         [Fact]
@@ -48,15 +46,33 @@ namespace TestTimeSeriesForecasting
         }
 
         [Fact]
-        public void TestElements()
+        public void TestInputElements()
         {
-            int batch = 0; int row = 0; int col = 0;
-            Assert.True(Math.Abs((double)_set.Rows[GetRow(batch, row)].ItemArray[GetColumn(col)]! - 
-                _input[batch, row, col].item<float>()) < _fixture.Tolerance);
-            batch = 20; row = 7; col = 1;
-            double expected = (double)_set.Rows[GetRow(batch, row)].ItemArray[GetColumn(col)]!;
-            double actual = _input[batch, row, col].item<float>();
-            Assert.True(Math.Abs(expected - actual) < _fixture.Tolerance);
+            var rnd = new Random(5678);
+            for (int i = 0; i < 500; i++)
+            {
+                int batch = rnd.Next(0, _batches);
+                int row = rnd.Next(0, InputWidth);
+                int col = rnd.Next(0, _features);
+                double expected = (double)_set.Rows[GetRow(batch, row)].ItemArray[GetColumn(col)]!;
+                double actual = _input[batch, row, col].item<float>();
+                Assert.True(Math.Abs(expected - actual) < _fixture.Tolerance);
+            }
+        }
+
+        [Fact]
+        public void TestOutputElements()
+        {
+            var rnd = new Random(3456);
+            for (int i = 0; i < 500; i++)
+            {
+                int batch = rnd.Next(0, _batches);
+                int row = rnd.Next(0, LabelWidth);
+                int col = rnd.Next(0, _labels);
+                double expected = (double)_set.Rows[GetRow(batch, row)].ItemArray[GetColumn(col)]!;
+                double actual = _input[batch, row, col].item<float>();
+                Assert.True(Math.Abs(expected - actual) < _fixture.Tolerance);
+            }
         }
 
         private static int GetRow(int batch, int row) => batch + row;
