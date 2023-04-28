d1e0f09f72bd57001472393c399760237efc4b4f
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-04-28 15:38:19 +0200

    Add ReLU activation function to Baseline model

--- RealTests/TimeSeriesForecasting.NeuralNetwork/Baseline.cs
+++ RealTests/TimeSeriesForecasting.NeuralNetwork/Baseline.cs
@@ -18,7 +18,7 @@ namespace TimeSeriesForecasting.NeuralNetwork
             // Input tensor is a 3D Tensor of shape (batch_size, observations, features).
             Tensor flattenedInput = input.flatten(start_dim: 1);
             // Flattened input Tensor has shape (batch_size, observations * features), so it is compatible with the linear layer.
-            return _linear.forward(flattenedInput).type_as(input);
+            return functional.relu(_linear.forward(flattenedInput).type_as(input));
         }
     }
 }
