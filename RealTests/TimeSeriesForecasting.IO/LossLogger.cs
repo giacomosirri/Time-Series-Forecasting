40623c08ffd3a49ea5c7b21d14348c4a71db1ff8
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-04-28 12:55:57 +0200

    Add LossLogger implementation

---
+++ RealTests/TimeSeriesForecasting.IO/LossLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeSeriesForecasting.IO
+{
+    public class LossLogger : Logger<(int index, float value)>
+    {
+        public LossLogger(string filePath) : base(filePath) { }
+
+        protected override string ValueRepresentation((int index, float value) value) => $"Epoch {value.index}: Loss = {value.value}";
+    }
+}
