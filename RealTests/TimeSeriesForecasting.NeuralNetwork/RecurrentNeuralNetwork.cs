d40fca3b1d15aba58f032cd767bd8c4d9fda3445
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-05-03 21:44:39 +0200

    Add constructor that loads parameters from file for neural network models

--- RealTests/TimeSeriesForecasting.NeuralNetwork/RecurrentNeuralNetwork.cs
+++ RealTests/TimeSeriesForecasting.NeuralNetwork/RecurrentNeuralNetwork.cs
@@ -22,12 +22,22 @@ namespace TimeSeriesForecasting.NeuralNetwork
         private readonly RNN _rnn;
         private readonly Linear _linear;
 
+        /*
+         * Create a new RNN model with weights and biases initialized with this distribution:
+         * U(−sqrt(k), sqrt(k)), where k = 1 / hidden_size.
+         */
         public RecurrentNeuralNetwork(long inputFeatures, long outputTimeSteps, long outputFeatures) : base(nameof(RNN))
         {
             _rnn = RNN(inputFeatures, HiddenSize, numLayers: Layers, batchFirst: true);
             _linear = Linear(HiddenSize, outputFeatures);
             RegisterComponents();
         }
+
+        /*
+         * Initialize the model loading its weights and biases from the given file.
+         */
+        public RecurrentNeuralNetwork(long inputFeatures, long outputTimeSteps, long outputFeatures, string path) 
+            : this(inputFeatures, outputTimeSteps, outputFeatures) => load(path);
 
         public override Tensor forward(Tensor input)
         {
--- RealTests/TimeSeriesForecasting.NeuralNetwork/SimpleNeuralNetwork.cs
+++ RealTests/TimeSeriesForecasting.NeuralNetwork/SimpleNeuralNetwork.cs
@@ -8,11 +8,22 @@ namespace TimeSeriesForecasting.NeuralNetwork
     {
         private readonly Linear _linear;
 
-        public SimpleNeuralNetwork(long inputTimeSteps, long inputFeatures, long outputTimeSteps, long outputFeatures) : base(nameof(SimpleNeuralNetwork))
+        /*
+         * Create a new RNN model with weights and biases initialized with this distribution:
+         * U(−sqrt(k), sqrt(k)), where k = 1 / input_features.
+         */
+        public SimpleNeuralNetwork(long inputTimeSteps, long inputFeatures, long outputTimeSteps, long outputFeatures) 
+            : base(nameof(SimpleNeuralNetwork))
         {
             _linear = Linear(inputTimeSteps * inputFeatures, outputTimeSteps * outputFeatures);
             RegisterComponents();
         }
+
+        /*
+         * Initialize the model loading its weights and biases from the given file.
+         */
+        public SimpleNeuralNetwork(long inputTimeSteps, long inputFeatures, long outputTimeSteps, long outputFeatures, string path)
+            : this(inputTimeSteps, inputFeatures, outputTimeSteps, outputFeatures) => load(path);
 
         public override Tensor forward(Tensor input)
         {
