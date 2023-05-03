b80ffe7fb5f24749c149bb7c8d444fa4c7b7248d
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-05-03 14:29:34 +0200

    Remove ReLU activation function from SimpleNeuralNetwork model

--- RealTests/TimeSeriesForecasting.NeuralNetwork/SimpleNeuralNetwork.cs
+++ RealTests/TimeSeriesForecasting.NeuralNetwork/SimpleNeuralNetwork.cs
@@ -16,10 +16,10 @@ namespace TimeSeriesForecasting.NeuralNetwork
 
         public override Tensor forward(Tensor input)
         {
-            // Input tensor is a 3D Tensor of shape (batch_size, observations, features).
+            // Input tensor is a 3D Tensor of shape (batch_size, time_steps, features).
             Tensor flattenedInput = input.flatten(start_dim: 1);
-            // Flattened input Tensor has shape (batch_size, observations * features), so it is compatible with the linear layer.
-            return functional.relu(_linear.forward(flattenedInput).type_as(input));
+            // Flattened input Tensor has shape (batch_size, time_steps * features), so it is compatible with the linear layer.
+            return _linear.forward(flattenedInput).type_as(input);
         }
     }
 }
