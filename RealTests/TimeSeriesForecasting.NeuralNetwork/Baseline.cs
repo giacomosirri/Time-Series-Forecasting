b1fa32312bdf2c34ce1621b0b350e761f380ab7f
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-05-07 21:56:40 +0200

    Create new models Baseline and LSTM

---
+++ RealTests/TimeSeriesForecasting.NeuralNetwork/Baseline.cs
@@ -0,0 +1,17 @@
+﻿using TorchSharp;
+using static TorchSharp.torch;
+
+namespace TimeSeriesForecasting.NeuralNetwork
+{
+    internal class Baseline : NetworkModel
+    {
+        public Baseline() : base(nameof(Baseline))
+        {
+        }
+
+        public override Tensor forward(torch.Tensor input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealTests/TimeSeriesForecasting.NeuralNetwork/LSTM.cs
@@ -0,0 +1,17 @@
+﻿using TorchSharp;
+using static TorchSharp.torch;
+
+namespace TimeSeriesForecasting.NeuralNetwork
+{
+    internal class LSTM : NetworkModel
+    {
+        public LSTM() : base(nameof(LSTM))
+        {
+        }
+
+        public override Tensor forward(torch.Tensor input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
