7d90bd9a283c2c4420f3c8cfbf3db3c9c8299770
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-05-12 10:56:44 +0200

    Complete implementation of command line parsing

--- RealTests/TimeSeriesForecasting/Program.cs
+++ RealTests/TimeSeriesForecasting/Program.cs
@@ -1,16 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System.CommandLine;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace TimeSeriesForecasting
 {
-    // The available running modes for this program.
-    internal enum RunningMode
-    {
-        TEST,
-        TRAIN,
-        PREDICT
-    }
-
     /*
      * This class encapsulates all the global configuration parameters.
      * These parameters are needed for every running mode, as the input to both the training 
@@ -35,43 +29,20 @@ namespace TimeSeriesForecasting
         public int Offset { internal get; set; }
     }
 
+
     internal class Program
     {
-        internal static RunningMode Mode { get; set; }
+        internal const string Completion = "  COMPLETE\n";
+        private const string IOErrorMessage = "An error occurred when reading a directory or file. The program has been stopped.";
+        private const string DirectoryErrorMessage = "The directory you provided is not valid. The program has been stopped.";
+
         internal static bool IsLogEnabled { get; private set; }
 
-        // To be removed.
-        private static readonly string ValuesFile = Properties.Resources.NumericDatasetParquetFilePath;
-        private static readonly string DatesFile = Properties.Resources.TimestampDatasetParquetFilePath;
-        internal static readonly string LogDir = Properties.Resources.LogDirectoryPath;
-
-        internal const string Completion = "  COMPLETE\n";
-
-        internal static GlobalConfiguration GlobalConfiguration { get; private set; } = new GlobalConfiguration();
-        internal static string LogDirPath { get; private set; } = "";
-
         /*
-         * This is the starting point of program execution. Initially, the program loads data of interest from file,
-         * then it preprocesses it and create the necessary data structures to perform the analysis.
-         * After that, there is a fork between the code that trains a neural network model and the code that predicts
-         * new values using a pre-trained model. 
          * 
-         * This is for two main reasons:
-         * - Separation of concerns: Training a model and predicting a time series are two very different operations
-         * and they are not even necessarily consequential. In fact, one can train a model, save its parameters on
-         * file and then use those parameters on a completely different software. Conversely, the prediction can be
-         * made using parameters read from a file.
-         * - Efficiency: The prediction of new values is a much more frequent operation than the training of a model.
-         * 
-         * This means that this program can be run either in training mode or in prediction mode.
-         * To choose which to run, specify the correct command-line argument. There are three numerics arguments allowed:
-         * - 0: Train the model specified in the configurationSettings.json file and save its learnt parameters on file.
-         * - 1: Predict new values using the model parameters read from file.
-         * Currently, this operation merely tries to predict the actual values of the test set, which are already known. 
-         * This is obviously done for debugging purposes, but it will be changed in the final implementation.
-         * - 2: Perform operation 0 and 1 subsequently, so that the full model lifecycle is mimicked.
-         * Any other command-line argument will cease the program.
          */
+        internal static GlobalConfiguration GlobalConfiguration { get; private set; } = new GlobalConfiguration();
+
         static void Main(string[] args)
         {
             DateTime startTime = DateTime.Now;
@@ -80,14 +51,15 @@ namespace TimeSeriesForecasting
             // Create command line.
             var rootCommand = new RootCommand("App that creates, trains and runs a neural network for time series forecasting.");
 
-            /*
-             * Create command "train", add its options and arguments and set its behavior.
-             */
+            // Create command "train".
             var trainCommand = new Command("train", "Trains the neural network, i.e. changes its parameters according to the provided data, " +
-                "but does not test the new trained model.");
-            var trainLogOption = new Option<bool>("--log", "if true, some output that aids the understanding of the training process is created.")
+                                                    "but does not test the new trained model on the test set.");
+            var trainLogOption = new Option<bool>(
+                name: "--log",
+                description: "if true, outputs some data and charts useful to understand the result of the training process."
+            )
             {
-                IsRequired = false,
+                IsRequired = false
             };
             trainLogOption.SetDefaultValue(true);
             trainLogOption.AddAlias("--l");
@@ -96,76 +68,89 @@ namespace TimeSeriesForecasting
             trainCommand.AddArgument(trainArgument);
             trainCommand.SetHandler((bool log, string inputDirectoryPath) =>
             {
-                // Sets the running mode and the log enabling based on the command and its log option.
-                Mode = RunningMode.TRAIN;
                 IsLogEnabled = log;
                 try
                 {                
                     // Calculate the asbolute path of the input directory.
                     string userInputDir = Path.GetFullPath(inputDirectoryPath);
                     // Then try to read the config file inside the directory. If not present, an exception is thrown.
-                    CreateGlobalConfiguration(userInputDir);
+                    GlobalConfiguration = GetGlobalConfiguration(userInputDir);
                     // Finally execute the train command.
                     ProgramTrain.ExecuteTrainCommand(userInputDir);
                 }
                 catch (Exception ex) when (ex is IOException)
                 {
-                    Console.WriteLine("An unexpected error happened when reading a directory or file. The program is stopped.");
+                    Console.WriteLine(IOErrorMessage);
                     Environment.Exit(1);
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("The given path is not a valid directory, so the program cannot run.");
+                    Console.WriteLine(DirectoryErrorMessage);
                     Environment.Exit(1);
                 }
             }, trainLogOption, trainArgument);
 
-            /*
-             * Create command "test", add its options and arguments and set its behavior.
-             */
-            var testCommand = new Command("train", "Trains the neural network, i.e. changes its parameters according to the provided data, " +
-                "but does not test the new trained model.");
-            var testLogOption = new Option<bool>("--log", "if true, some output that aids the understanding of the training process is created.")
-            {
-                IsRequired = false
-            };
-            testLogOption.AddAlias("--l");
-            testLogOption.SetDefaultValue(true);
-            testCommand.AddOption(testLogOption); 
+            // Create command 'test'.
+            var testCommand = new Command("train", "Trains the neural network and tests it on the test set, " +
+                                                   "providing output useful to understand the quality of the model.");
             var testArgument = new Argument<string>("input", "The relative or absolute path of the input directory.");
             testCommand.AddArgument(testArgument);
-            testCommand.SetHandler((bool log, string inputDirectoryPath) =>
+            testCommand.SetHandler((string inputDirectoryPath) =>
             {
-                // Sets the running mode and the log enabling based on the command and its log option.
-                Mode = RunningMode.TEST;
-                IsLogEnabled = log;
+                // In test mode the log is always enabled, because testing a model and getting no output would not make any sense.
+                IsLogEnabled = true;
                 try
                 {
                     // Calculate the asbolute path of the input directory.
                     string userInputDir = Path.GetFullPath(inputDirectoryPath);
                     // Then try to read the config file inside the directory. If not present, an exception is thrown.
-                    CreateGlobalConfiguration(userInputDir);
-                    ProgramTrain.ExecuteTrainCommand(userInputDir);
+                    GlobalConfiguration = GetGlobalConfiguration(userInputDir);
+                    ProgramTest.ExecuteTestCommand(userInputDir);
                 }
                 catch (Exception ex) when (ex is IOException)
                 {
-                    Console.WriteLine("An unexpected error happened when reading a directory or file. The program is stopped.");
+                    Console.WriteLine(IOErrorMessage);
                     Environment.Exit(1);
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("The given path is not a valid directory, so the program cannot run.");
+                    Console.WriteLine(DirectoryErrorMessage);
                     Environment.Exit(1);
                 }
-            }, trainLogOption, trainArgument);
+            }, testArgument);
 
+            // Create command "predict".
             var predictCommand = new Command("predict", "Predicts future values using a trained neural network.");
             var predictArgument = new Argument<string>("input", "The relative or absolute path of the input directory.");
             predictCommand.AddArgument(predictArgument);
+            predictCommand.SetHandler((string inputDirectoryPath) =>
+            {
+                IsLogEnabled = true;
+                try
+                {
+                    // Calculate the asbolute path of the input directory.
+                    string userInputDir = Path.GetFullPath(inputDirectoryPath);
+                    // Then try to read the config file inside the directory. If not present, an exception is thrown.
+                    GlobalConfiguration = GetGlobalConfiguration(userInputDir);
+                    ProgramPredict.ExecutePredictCommand(userInputDir);
+                }
+                catch (Exception ex) when (ex is IOException)
+                {
+                    Console.WriteLine(IOErrorMessage);
+                    Environment.Exit(1);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(DirectoryErrorMessage);
+                    Environment.Exit(1);
+                }
+            }, testArgument);
 
+            // Add the new commands to the root command.
             rootCommand.AddCommand(trainCommand);
             rootCommand.AddCommand(testCommand);
             rootCommand.AddCommand(predictCommand);
+            // Process the command line and invoke the command selected by the user.
             rootCommand.Invoke(args);
 
             DateTime endTime = DateTime.Now;
@@ -176,10 +161,10 @@ namespace TimeSeriesForecasting
         }
 
         /*
-         * This method creates and returns a global configuration object from the configuration file
-         * found inside the user directory. A InvalidArgumentException is raised if no such file is present.
+         * This method creates and returns a global configuration object deserializing the configuration file
+         * found inside the user directory. An ArgumentException is thrown if no such file is present.
          */
-        private static GlobalConfiguration CreateGlobalConfiguration(string inputDirectoryPath)
+        private static GlobalConfiguration GetGlobalConfiguration(string inputDirectoryPath)
         {
             try
             {
@@ -198,6 +183,34 @@ namespace TimeSeriesForecasting
             {
                 throw new ArgumentException("The given path is not a directory or does not contain a file named 'global_config.json'.");
             }
+        }
+
+        /*
+         * This method attemps to draw a graph using python's matplotlib library.
+         * The input is the name of a python script that is included in the source files of this project.
+         */
+        internal static (bool result, string? message) RunPythonScript(string scriptName, string outputDirectoryPath)
+        {
+            if ((Environment.GetEnvironmentVariable("PATH") != null) &&
+                Environment.GetEnvironmentVariable("PATH")!.Contains("Python"))
+            {
+                string scriptPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, scriptName);
+                // Create and execute a new python process to draw the graph.
+                var process = new Process();
+                process.StartInfo.FileName = "python";
+                // The arguments are the name of the script and the path of the directory where the data is located.
+                process.StartInfo.Arguments = $"{scriptPath} {outputDirectoryPath}";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                bool res = process.Start();
+                if (!res)
+                {
+                    return (false, "Python script could not be executed.");
+                }
+                process.WaitForExit();
+                return (true, null);
+            }
+            else return (false, "  Python is not installed on your system. Please install it and try again.\n");
         }
     }
 }
