cd513a8c498ea512201dcf86f108a5ffb8a25340
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-05-12 10:25:45 +0200

    Create ProgramTest class

---
+++ RealTests/TimeSeriesForecasting/ProgramTest.cs
@@ -0,0 +1,14 @@
+﻿namespace TimeSeriesForecasting
+{
+    internal class ProgramTest
+    {
+        /*
+         * The test command is just the train with some additional operations performed on the same dataset,
+         * so it makes sense to defer the test operations to the same method that trains the model.
+         * Avoiding this would cause the duplication of all the preliminary code that prepares the dataset for
+         * training and testing, as the latter still needs the input and expected output to be retrieved from 
+         * the dataset provided by the user, using the same split ratios too.
+         */
+        internal static void ExecuteTestCommand(string inputDirectoryPath) => ProgramTrain.ExecuteTrainCommand(inputDirectoryPath, true); 
+    }
+}
