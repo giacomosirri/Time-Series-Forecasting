708f7c9158c810d96b3fa9441862d01a768da41c
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-05-16 10:26:29 +0200

    Update Record to make it more general purpose

--- RealTests/TimeSeriesForecasting.IO/Record.cs
+++ RealTests/TimeSeriesForecasting.IO/Record.cs
@@ -1,24 +1,20 @@
-﻿namespace TimeSeriesForecasting.IO
+﻿using Newtonsoft.Json.Linq;
+
+namespace TimeSeriesForecasting.IO
 {
     public class Record
     {
-        public static IDictionary<string, Tuple<double, double>> ValueRanges { get; } = new Dictionary<string, Tuple<double, double>> 
-        {
-            { "mbar", Tuple.Create(0.0, 1150.0) },
-            { "degC", Tuple.Create(-50.0, 50.0) },
-            { "K", Tuple.Create(220.0, 325.0) },
-            { "%", Tuple.Create(0.0, 100.0) },
-            { "g/kg", Tuple.Create(0.0, 30.0) },
-            { "mmol/mol", Tuple.Create(0.0, 40.0) },
-            { "g/m**3", Tuple.Create(800.0, 1500.0) },
-            { "m/s", Tuple.Create(0.0, 70.0) },
-            { "deg", Tuple.Create(0.0, 360.0) }
-        };
+        /*
+         * This dictionary maps a feature name to a pair of doubles that represent the minimum and maximum
+         * values that can be assumed by an instance of that feature.
+         */
+        public static IDictionary<string, (double? minValue, double? maxValue)> ValueRanges { get; } =
+            new Dictionary<string, (double? minValue, double? maxValue)>();
 
-        /// <summary>
-        /// Returns the name of the column that acts as index for a record, similar to an index in a Pandas' Series or DataFrame.
-        /// Such column must be of type <see cref="DateTime"/>, as these record represent time steps in a time series.
-        /// </summary>
+        /* 
+         * Returns the name of the column that acts as index for a record, similar to an index in a Pandas' Series or DataFrame.
+         * Such column must be of type DateTime, as these record represent time steps in a time series.
+         */
         public static string Index { get; set; } = "";
 
         public IDictionary<string, double> Features { get; private set; }
@@ -35,25 +31,37 @@
             Features = values.Keys.Zip(values.Values.Select(elem => elem!.Value).ToList(), 
                 (k,v) => new {Key = k, Value = v}).ToDictionary(x => x.Key, x => x.Value);
         }
-        
-        public double[] GetNumericValues() => Features.Values.ToArray();
 
-        public static string? GetUnitOfMeasureFromFeatureName(string feature)
+        /*
+         * Reads the features value ranges from a json file. Returns true if the operation is successful, false otherwise.
+         */
+        public static bool ReadValueRangesFromJsonFile(string filePath)
         {
             try
             {
-                var str = feature.Split('(').Last().Split(")").First();
-                return str == "" || str == feature ? null : str;
-            }
-            catch (InvalidOperationException)
+                JObject obj = JObject.Parse(File.ReadAllText(filePath));
+                foreach (var key in obj)
+                {
+                    string feature = key.Key;
+                    if (key.Value != null)
+                    {
+                        double? minValue = (double?)key.Value[0];
+                        double? maxValue = (double?)key.Value[1];
+                        ValueRanges[feature] = (minValue, maxValue);
+                    }
+                    else return false;
+                }
+                return true;
+            } 
+            catch (Exception)
             {
-                return null;
+                return false;
             }
         }
 
         public override string ToString()
         {
-            return $"{TimeStamp}:\n" + string.Join(",", Features.Select(i => $"{i.Key}: {i.Value}")) + "\n";
+            return $"{TimeStamp}:\n" + string.Join("\n", Features.Select(i => $"{i.Key}: {i.Value}")) + "\n";
         }
     }
 }
